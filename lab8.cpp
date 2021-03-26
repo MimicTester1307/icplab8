58ac50630e551e1035e2905f6fad17257b45e129
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2021-03-26 21:55:54 +0000

    add functions to display different multidimensional matrices

--- lab8.cpp
+++ lab8.cpp
@@ -7,6 +7,9 @@ using namespace std;
 int linearIndx(int, int, int);
 int inverseIndx(int);
 int generateRandomNumber();
+void generateAndPrintFirstMatrix();
+void generateAndPrintSecondMatrix();
+void generateAndPrintThirdMatrix();
 
 int main() {
   srand(time(nullptr));   // set initial seed value to 5323
@@ -44,6 +47,15 @@ int main() {
     }
   }
 
+  /*Section for generating 3 lower triangular arrays */
+  //Displaying 8x8 array
+  generateAndPrintFirstMatrix();
+
+  //Display 32x32 array
+  generateAndPrintSecondMatrix();
+
+  //Display 128x128 generateAndPrintFirstMatrix
+  generateAndPrintThirdMatrix();
 }
 
 
@@ -56,7 +68,80 @@ int inverseIndx(int index) {
   return 0;
 }
 
+//Function to generate a random number
 int generateRandomNumber() {
   int randomNumber = rand() % 1000 + 1;
   return randomNumber;
+}
+
+//Function to generate and display 8x8 matrix
+void generateAndPrintFirstMatrix() {
+  int multiDimensionalArray[8][8] = {};
+  //generating the array
+  for (int i = 0; i < 8; i++) {
+    for (int j = 0; j < 8; j++) {
+      if(i >= j) {
+        multiDimensionalArray[i][j] = generateRandomNumber();
+      } else {
+        multiDimensionalArray[i][j] = 0;
+      }
+    }
+    cout << endl;
+  }
+
+  //printing out the array
+  for (int i = 0; i < 8; i++) {
+    for(int j = 0; j < 8; j++) {
+      cout << multiDimensionalArray[i][j] << " ";
+    }
+    cout << endl;
+  }
+}
+
+//Function to generate and display 32x32 matrix
+void generateAndPrintSecondMatrix() {
+  int multiDimensionalArray[32][32] = {};
+  //generating the array
+  for (int i = 0; i < 32; i++) {
+    for (int j = 0; j < 32; j++) {
+      if(i >= j) {
+        multiDimensionalArray[i][j] = generateRandomNumber();
+      } else {
+        multiDimensionalArray[i][j] = 0;
+      }
+    }
+    cout << endl;
+  }
+
+  //printing out the array
+  for (int i = 0; i < 32; i++) {
+    for(int j = 0; j < 32; j++) {
+      cout << multiDimensionalArray[i][j] << " ";
+    }
+    cout << endl;
+  }
+}
+
+//Function to generate and display 128x128 function
+void generateAndPrintThirdMatrix() {
+  int multiDimensionalArray[128][128] = {};
+  //generating the array
+  for (int i = 0; i < 128; i++) {
+    for (int j = 0; j < 128; j++) {
+      if(i >= j) {
+        multiDimensionalArray[i][j] = generateRandomNumber();
+      } else {
+        multiDimensionalArray[i][j] = 0;
+      }
+    }
+    cout << endl;
+  }
+
+  //printing out the array
+  for (int i = 0; i < 128; i++) {
+    for(int j = 0; j < 128; j++) {
+      cout << multiDimensionalArray[i][j] << " ";
+    }
+    cout << endl;
+  }
 }
