31112e9ae8dd273268ed8708d06464fc2b538c9b
MimicTester1307 <excel.chukwu@ashesi.edu.gh>
2021-03-26 23:21:55 +0000

    update inverse function

--- lab8.cpp
+++ lab8.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 //forward declarations
 int linearIndx(int, int, int);
-int inverseIndx(int);
+int inverseIndx(int, int);
 int generateRandomNumber();
 void generateAndPrintFirstMatrix();
 void generateAndPrintSecondMatrix();
@@ -21,7 +21,6 @@ int main() {
 
   //generating the multidimensional array and filling it in a lower triangular manner:
   int multiDimensionalArray[rows][columns];
-
   for (int i = 0; i < rows; i++) {
     for (int j = 0; j < columns; j++) {
       if(i >= j) {
@@ -33,6 +32,15 @@ int main() {
     cout << endl;
   }
 
+  //printing the first 20 elements of the lower triangular array
+  for (int i = 0; i < 20; i++) {
+    for(int j = 0; j < 20; j++) {
+      if (multiDimensionalArray[i][j] != 0) {
+        cout << multiDimensionalArray[i][j] << " at index (" << i << "," << j << ")"  << endl;
+      }
+     }
+  }
+
 
   // declaring the linear array
   int linearArray[rows + columns] = {};
@@ -41,7 +49,7 @@ int main() {
   for (int i = 0; i < rows; i++) {
     for (int j = 0; j < columns; j++) {
       if(multiDimensionalArray[i][j] != 0){
-        cout << "The position of element " << multiDimensionalArray[i][j] << " from the multidimensional array is " << linearArray[0] + linearIndx(i, j, columns) << " in the linear array." << endl;
+        //cout << "The position of element " << multiDimensionalArray[i][j] << " from the multidimensional array is " << linearArray[0] + linearIndx(i, j, columns) << " in the linear array." << endl;
         linearArray[linearArray[0] + linearIndx(i, j, columns)] = multiDimensionalArray[i][j];
       }
     }
@@ -49,23 +57,28 @@ int main() {
 
   /*Section for generating 3 lower triangular arrays */
   //Displaying 8x8 array
-  generateAndPrintFirstMatrix();
+  //generateAndPrintFirstMatrix();
 
   //Display 32x32 array
-  generateAndPrintSecondMatrix();
+  //generateAndPrintSecondMatrix();
 
   //Display 128x128 generateAndPrintFirstMatrix
-  generateAndPrintThirdMatrix();
+  //generateAndPrintThirdMatrix();
 }
 
 
+//Function to calculate the corresponding linear position of the multidimensional array
 int linearIndx(int rowIndex, int columnIndex, int numberOfColumns) {
   int I = rowIndex * numberOfColumns + columnIndex;
   return I;
 }
 
-int inverseIndx(int index) {
-  return 0;
+//function to calculate the corresponding multidimensional position from the linear array
+int inverseIndx(int index, int number_of_columns) {
+  int number of
+  for (int i = index; i > 0; i--) {
+    if
+  }
 }
 
 //Function to generate a random number
@@ -75,7 +88,7 @@ int generateRandomNumber() {
 }
 
 //Function to generate and display 8x8 matrix
-void generateAndPrintFirstMatrix() {
+//void generateAndPrintFirstMatrix() {
   int multiDimensionalArray[8][8] = {};
   //generating the array
   for (int i = 0; i < 8; i++) {
@@ -99,7 +112,7 @@ void generateAndPrintFirstMatrix() {
 }
 
 //Function to generate and display 32x32 matrix
-void generateAndPrintSecondMatrix() {
+//void generateAndPrintSecondMatrix() {
   int multiDimensionalArray[32][32] = {};
   //generating the array
   for (int i = 0; i < 32; i++) {
@@ -113,9 +126,9 @@ void generateAndPrintSecondMatrix() {
     cout << endl;
   }
 
-  //printing out the array
-  for (int i = 0; i < 32; i++) {
-    for(int j = 0; j < 32; j++) {
+  //printing out the array (first 10 values)
+  for (int i = 0; i < 10; i++) {
+    for(int j = 0; j < 10; j++) {
       cout << multiDimensionalArray[i][j] << " ";
     }
     cout << endl;
@@ -123,7 +136,7 @@ void generateAndPrintSecondMatrix() {
 }
 
 //Function to generate and display 128x128 function
-void generateAndPrintThirdMatrix() {
+//void generateAndPrintThirdMatrix() {
   int multiDimensionalArray[128][128] = {};
   //generating the array
   for (int i = 0; i < 128; i++) {
@@ -137,9 +150,9 @@ void generateAndPrintThirdMatrix() {
     cout << endl;
   }
 
-  //printing out the array
-  for (int i = 0; i < 128; i++) {
-    for(int j = 0; j < 128; j++) {
+  //printing out the array (first 10 values)
+  for (int i = 0; i < 10; i++) {
+    for(int j = 0; j < 10; j++) {
       cout << multiDimensionalArray[i][j] << " ";
     }
     cout << endl;
